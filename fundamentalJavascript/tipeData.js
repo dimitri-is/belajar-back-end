0615222b8bcf15ce3f8b1b15c93e8fe2e74f3097
Dimitri Irfan <dimitriirfan08@gmail.com>
2022-12-31 16:00:13 +0800

    Menambahkan, dan juga mempraktekkan penggunaan tipe data number, serta penggunaan pada operator aritmatika. Mulai dari penjumlahan sampai perpangkatan.

--- fundamentalJavascript/tipeData.js
+++ fundamentalJavascript/tipeData.js
@@ -11,3 +11,32 @@ let a;
 console.log(typeof (a));
 // maka hasilnya akan keluar juga=
 // tulisan undefinel yang lebih jelas
+
+// numbers
+// tipe data angka, yang dituliskan dengan angka seperti pada umumnya
+let o = 10;
+console.log(typeof (o));
+let y = 12.08;
+console.log(typeof (y));
+// akan memberikan hasil =
+// number
+// number
+
+// pembuatan contoh hitungan aritmatika, dan juga penggunaan tiap-tiap operatornya
+let n = 15;
+let m = 3;
+
+console.log(n + m);
+console.log(n - m);
+console.log(n * m);
+console.log(n / m);
+console.log(n % m);
+console.log(n ** m);
+// akan menghasilkan =
+// 18 hasil penjumlahan
+// 12 hasil pengurangan
+// 45 hasil perkalian
+// 5 hasil pembagian
+// 0 hasil dari sisa hasil bagi
+// 3375 hasil perpangkatan
+

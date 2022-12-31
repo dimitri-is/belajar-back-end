e7aa2d1590d9f0169bb89751d58a94cbc1001e23
Dimitri Irfan <dimitriirfan08@gmail.com>
2022-12-31 15:40:38 +0800

    Memberikan contoh tipe data undefined yang tidak akan memiliki nilai pada variabel

---
+++ fundamentalJavascript/tipeData.js
@@ -0,0 +1,13 @@
+// undefined
+// variabel yang tidak memiliki nilai
+//contoh pertama 
+let x;
+console.log(x);
+// maka akan muncul hasil=
+// undefined
+
+// contoh kedua jika ditambahkan fungsi typeof() akan menghasilkan output undefined yang lebih jelas
+let a;
+console.log(typeof (a));
+// maka hasilnya akan keluar juga=
+// tulisan undefinel yang lebih jelas

2a674ff3ec31ed773fa30b8df49c3bc8353a020b
Dimitri Irfan <dimitriirfan08@gmail.com>
2022-12-31 15:06:24 +0800

    Memberikan 2 contoh penerapan expression pada file pembelajaran variable.js

--- fundamentalJavascript/variable.js
+++ fundamentalJavascript/variable.js
@@ -12,3 +12,16 @@ let lastName = "Penceng";
 console.log(lastName);
 // maka hasilnya akan sama seperti pada contoh pertama yaitu=
 // Penceng
+
+// contoh penerapan expression 1
+let jenengAwal = "Safa";
+let jenengLengkap = (jenengAwal + "Ndaru");
+
+console.log(jenengLengkap);
+// hasilnya akan SafaNdaru, karena di dalam tanda petik tidak ada spasi di awal kata.
+
+// contoh penerapan expression 2
+let nameLengkap = "Dimitri" + "Irfan";
+
+console.log(nameLengkap);
+// hasil penerapan expression 2, menjadi DimitriIrfan, dan kasusnya seperti pada contoh expression pertama

0a64adcb9128d3d3abbba69dd8ceaa2f2c18cd35
Dimitri Irfan <dimitriirfan08@gmail.com>
2022-12-31 14:52:20 +0800

    Telah memberikan contoh penggunaan variable let, dan terdapat dua contoh penerapannya.

---
+++ fundamentalJavascript/variable.js
@@ -0,0 +1,14 @@
+let namaAkhir;
+namaAkhir = "Saputra";
+
+console.log(namaAkhir);
+
+// outputnya akan tertera tulisan=
+// Saputra
+
+// bisa juga memakai versi ini
+let lastName = "Penceng";
+
+console.log(lastName);
+// maka hasilnya akan sama seperti pada contoh pertama yaitu=
+// Penceng

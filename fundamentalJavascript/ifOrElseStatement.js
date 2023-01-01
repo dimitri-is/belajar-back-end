9f4779dc9bd107c4899497d26cc2eb6fc69db8cc
Dimitri Irfan <dimitriirfan08@gmail.com>
2023-01-01 19:37:31 +0800

    Telah menambahkan dasar penerapan ifElseStatement

---
+++ fundamentalJavascript/ifOrElseStatement.js
@@ -0,0 +1,16 @@
+/*statement if akan menguji suatu kondisi
+jika bermilai true, maka blok kode di dalamnya akan dijalankan. sebaliknya, jika bernilai false maka prosesnya akan dilewatkan.
+*/
+// contoh kodenya
+const sedangHujan = false;
+
+console.log("Persiapan sebelum berangkat kegiatan.");
+if (sedangHujan) {
+    console.log("Hari ini hujan, bawa payung.");
+}
+console.log("Berangkat kegiatan.");
+/*output    >
+Persiapan sebelum berangkat kegiatan.
+// jika nilai tertulis true, maka akan nampak juga "Hari ini hujan, bawa payung."
+Berangkat kegiatan.
+ */

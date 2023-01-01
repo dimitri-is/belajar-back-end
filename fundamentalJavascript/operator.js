a311937e7986e417e88c19b110a26ec97acb0deb
Dimitri Irfan <dimitriirfan08@gmail.com>
2023-01-01 11:08:21 +0800

    Memberikan contoh dasar penerapan operator assigment

---
+++ fundamentalJavascript/operator.js
@@ -0,0 +1,9 @@
+// fungsi operator adalah symbol yang memberitahu interpreter melakukan operasi aritmatika, relasi bahkan logika guna memberikan hasil tertentu
+// assigment operator
+// digunakan memberikan nilai variabel
+// tanda (=) operatornya yang digunakan untuk menginisialisasi nilai pada variabel
+// variabel yang ingin diberi nilai berada di sebelah kiri, di kanan merupakan nilainya, dan diantaranya merupakan operator assigment sendiri
+let y = "2";
+x = y;
+console.log(x);
+// nah berikut merupakan contohnya yang diartikan kita menginisiasikan nilai y pada variabel x, dan sekarang nilai x memiliki nilai yang sama dengan y
